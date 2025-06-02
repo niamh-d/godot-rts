1e633334023465e6af7be2bbf3280f2a96ff545c
Niamh <niamh.linguist@gmail.com>
2025-06-02 17:18:16 +0300

    global script

---
+++ globals/global.gd
@@ -0,0 +1,46 @@
+extends Node
+
+var add_nav_spot = PackedVector2Array()
+var add_to_nav_spot = PackedVector2Array()
+var workers_selected := false
+var building_selected := false
+
+var food_count := 1200
+var gold_count := 1200
+var wood_count := 1200
+var pop_count := 0
+var max_pop_count := 0
+
+var new_worker_target = null
+var new_worker_target_type = null
+var new_worker_target_job = null
+var new_worker_target_id = null
+
+var enemy_units = 0
+var good_units = 0
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func reset_all_vars():
+	add_nav_spot = PackedVector2Array()
+	add_to_nav_spot = PackedVector2Array()
+	workers_selected = false
+	building_selected = false
+
+	food_count = 1200
+	gold_count = 1200
+	wood_count = 1200
+	pop_count = 0
+	max_pop_count = 0
+
+	new_worker_target = null
+	new_worker_target_type = null
+	new_worker_target_job = null
+	new_worker_target_id = null
+
+	enemy_units = 0
+	good_units = 0
